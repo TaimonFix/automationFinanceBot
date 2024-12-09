0779194fcf5a087ebeb8ace894d72e35d48f76ac
BratyshevTD <bratyshev2013@mail.ru>
2024-12-09 13:14:45 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/cs/automationFinanceBot/AutomationFinanceBotApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.automationFinanceBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationFinanceBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationFinanceBotApplication.class, args);
+	}
+
+}
